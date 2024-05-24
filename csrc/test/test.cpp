aa25c593d3cba7beb0268ede04d506d88a5aec49
SeanWangJS <wangxiongclous@gmail.com>
2024-05-25 07:04:59 +0800

    feat: add binding

--- csrc/test/test.cpp
+++ csrc/test/test.cpp
@@ -11,3 +11,13 @@ TEST(ops, test_add_int) {
     EXPECT_EQ(c, a + b);
 
 }
+
+TEST(ops, test_add_float) {
+
+    float a = 1.0;
+    float b = 2.0;
+    float c = ops::add<float>(a, b);
+
+    EXPECT_EQ(c, a + b);
+
+}
